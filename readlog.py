e6ea47b54c449aa08427d2b3ed40220e6d8d7bd0
Skactor <sk4ct0r@gmail.com>
2021-01-18 23:25:05 +0800

    Update readlog.py
    使用相对路径读取日志文件

--- readlog.py
+++ readlog.py
@@ -10,7 +10,7 @@ url = "/ws/ops/tasks/log/"
 async def main_logic(t):
     print("#######start ws")
     async with websockets.connect(t) as client:
-        await client.send(json.dumps({"task": "//opt/jumpserver/logs/jumpserver"}))
+        await client.send(json.dumps({"task": "../../logs/jumpserver"}))
         while True:
             ret = json.loads(await client.recv())
             print(ret["message"], end="")
@@ -20,7 +20,7 @@ async def read_gunicorn(t):
     print("#######start ws")
     clean_pattern = re.compile(r"^.+?/(?:v1/terminal/terminals/|health/).+?$", re.M | re.I)
     async with websockets.connect(t) as client:
-        await client.send(json.dumps({"task": "//opt/jumpserver/logs/gunicorn"}))
+        await client.send(json.dumps({"task": "../../logs/gunicorn"}))
         while True:
             ret = json.loads(await client.recv())
             print(clean_pattern.sub(ret["message"], ""), end="")
